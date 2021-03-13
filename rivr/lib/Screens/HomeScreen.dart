dcf5fe259439fddf0a01431d3543ea0a256dda3d
MeshaMakes <meshamakesthings@gmail.com>
2021-03-13 12:27:41 -0400

    Added new info containers (2/2), updated colors

--- rivr/lib/Screens/HomeScreen.dart
+++ rivr/lib/Screens/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rivr/Screens/RoomScreen.dart';
 import 'package:rivr/Utils/ColorsClass.dart' as colors;
@@ -48,7 +49,34 @@ class _HomeScreenState extends State<HomeScreen> {
       "info": "Share your digital content quickly with everyone by sharing your screen. The possibilities are limitless when it comes to what you want to share"
     },
   ];
-  
+
+  List<Map> _cardInfo = [
+    {
+      "icon" : Icons.whatshot_outlined,
+      "title" : "Free & Unlimited",
+    },
+    {
+      "icon" : Icons.wifi_tethering_outlined,
+      "title" : "Seamless Connections",
+    },
+    {
+      "icon" : Icons.wb_cloudy_outlined,
+      "title" : "Minimalist Design",
+    },
+    {
+      "icon" : Icons.widgets_outlined,
+      "title" : "Highly Responsive",
+    },
+    {
+      "icon" : Icons.lock_outline_rounded,
+      "title" : "Privacy & Security",
+    },
+    {
+      "icon" : Icons.group_add_outlined,
+      "title" : "Just Use It...",
+    },
+  ];
+
   @override
   void initState() {
     _dialogClass = DialogClass(background: colors.bg, buttonColor: colors.bgDark, buttonTextColor: colors.white, textColor: colors.white,);
@@ -146,7 +174,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 crossAxisAlignment: CrossAxisAlignment.center,
                                 children: [
-                                  Icon(_info[i]["icon"], size: 50.0, color: Colors.blue,),
+                                  Icon(_info[i]["icon"], size: 50.0, color: colors.blue,),
                                   TextView(
                                     text: _info[i]["title"],
                                     padding: EdgeInsets.only(bottom: 5.0),
@@ -173,28 +201,87 @@ class _HomeScreenState extends State<HomeScreen> {
                       height: 1.0,
                       color: colors.white,
                     ),
+                    Center(
+                      child: TextView(
+                        text: "DON’T MISS OUT ON THE RIVR EXPERIENCE ",
+                        color: colors.white,
+                        letterSpacing: 1.0,
+                        size: _size.width > 700 ? 30.0 : 25.0,
+                        fontWeight: FontWeight.w500,
+                        padding: EdgeInsets.symmetric(vertical: 40.0),
+                      ),
+                    ),
                     Padding(
-                        padding: EdgeInsets.symmetric(vertical: 30.0,),
+                      padding: EdgeInsets.only(bottom: 40.0),
+                      child: Align(
+                        alignment: Alignment.center,
                         child: Wrap(
                           direction: Axis.horizontal,
-                          alignment: WrapAlignment.center,
-                          spacing: _size.width > 700 ? 30.0 : 10.0, runSpacing: _size.width > 700 ? 30.0 : 10.0,
+                          alignment: WrapAlignment.spaceEvenly,
+                          runAlignment: WrapAlignment.center,
+                          spacing: _size.width > 700 ? 30.0 : 20.0, runSpacing: _size.width > 700 ? 30.0 : 20.0,
                           children: [
-                            TextView(
-                                text: "dmstudios268@gmail.com",
-                                color: colors.white,
-                                size: 18.0,
-                                fontWeight: FontWeight.w400,
-                                padding: EdgeInsets.only(right: 10.0)
-                            ),
-                            TextView(
-                                text: "Instagram",
-                                color: colors.white,
-                                size: 18.0,
-                                fontWeight: FontWeight.w400,
-                                padding: EdgeInsets.only(left: 10.0)
-                            ),
+                            for(int i = 0; i < _cardInfo.length; i++)
+                              Container(
+                                width: _size.width > 700 ? _size.width / 4.44: _size.width,
+                                //height: _size.width > 700 ? _size.width / 5.0 : _size.width / 2.5,
+                                padding: EdgeInsets.all(10.0),
+                                decoration: BoxDecoration(
+                                  color: colors.bgLight,
+                                  borderRadius: BorderRadius.circular(5.0)
+                                ),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Icon(_cardInfo[i]["icon"], size: 50.0, color: colors.darkPurple,),
+                                    Expanded(
+                                      child: TextView(
+                                        text: _cardInfo[i]["title"],
+                                        padding: EdgeInsets.only( left: 20.0),
+                                        color: colors.darkPurple,
+                                        size: 20.0,
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
                           ],
+                        ),
+                      ),
+                    ),
+
+                    Divider(
+                      height: 1.0,
+                      color: colors.white,
+                    ),
+                    Padding(
+                        padding: EdgeInsets.symmetric(vertical: 30.0,),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Wrap(
+                            direction: Axis.horizontal,
+                            alignment: WrapAlignment.center,
+                            spacing: _size.width > 700 ? 30.0 : 10.0, runSpacing: _size.width > 700 ? 30.0 : 10.0,
+                            children: [
+                              TextView(
+                                  text: "dmstudios268@gmail.com",
+                                  color: colors.white,
+                                  size: 18.0,
+                                  fontWeight: FontWeight.w400,
+                                  padding: EdgeInsets.only(right: 10.0)
+                              ),
+                              TextView(
+                                  text: "Instagram",
+                                  color: colors.white,
+                                  size: 18.0,
+                                  fontWeight: FontWeight.w400,
+                                  padding: EdgeInsets.only(left: 10.0)
+                              ),
+                            ],
+                          ),
                         )
                     ),
                   ],
--- rivr/lib/Utils/ColorsClass.dart
+++ rivr/lib/Utils/ColorsClass.dart
@@ -5,3 +5,5 @@ Color bg = Color.fromRGBO(30, 30, 30, 1.0);
 Color bgDark = Color.fromRGBO(15, 15, 15, 1.0);
 Color white = Color.fromRGBO(255, 255, 255, 1.0);
 Color red = Color.fromRGBO(223, 41, 41, 1.0);
+Color darkPurple = Color.fromRGBO(120, 91, 237, 1.0);
+Color blue = Color.fromRGBO(0, 240, 255, 1.0);
