cd8a160403a4a949a7695692476933008947397c
ArcherDiaz <gfinderprod@gmail.com>
2021-03-15 13:37:32 -0400

    created ChatWidget

---
+++ rivr/lib/Utils/ChatWidget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatWidget extends StatefulWidget {
+  @override
+  _ChatWidgetState createState() => _ChatWidgetState();
+}
+
+class _ChatWidgetState extends State<ChatWidget> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
