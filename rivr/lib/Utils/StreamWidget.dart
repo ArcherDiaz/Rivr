fd8ff6c7bf4848e6d21e97b9a531dad91baceb1a
MeshaMakes <meshamakesthings@gmail.com>
2021-03-31 14:21:56 -0400

    Major fixes

--- rivr/lib/Utils/StreamWidget.dart
+++ rivr/lib/Utils/StreamWidget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rivr/Utils/ColorsClass.dart' as colors;
 import 'package:sad_lib/CustomWidgets.dart';
@@ -26,34 +27,44 @@ class StreamWidget extends StatefulWidget {
 class _StreamWidgetState extends State<StreamWidget> {
 
   bool _isInverted;
+  double ratio;
+  double videoBoxHeight;
+  double videoBoxWidth;
 
   @override
   void initState() {
     _isInverted = false;
+    
+    videoBoxHeight = widget.state == SizeState.focused
+        ?  widget.focusedSize.height
+        : widget.state == SizeState.desktop ? widget.desktopSize.height : widget.mobileSize.height;
+
+    videoBoxWidth = widget.state == SizeState.focused
+        ? widget.focusedSize.width
+        : widget.state == SizeState.desktop ? widget.desktopSize.width : widget.mobileSize.width;
+    
     super.initState();
   }
 
+  @override
+  void didUpdateWidget(covariant StreamWidget oldWidget) {
+    if(widget.streamData["stream width"] != null && widget.streamData["stream height"] != null) {
+      ratio = widget.streamData["stream width"] / videoBoxWidth;
+      videoBoxHeight = widget.streamData["stream height"] / ratio;
+    }    
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
       duration: Duration(milliseconds: 750,),
-      width: widget.state == SizeState.focused
-          ? widget.focusedSize.width
-          : widget.state == SizeState.desktop ? widget.desktopSize.width : widget.mobileSize.width,
-      height: widget.state == SizeState.focused
-          ?  widget.focusedSize.height
-          : widget.state == SizeState.desktop ? widget.desktopSize.height : widget.mobileSize.height,
+      width: videoBoxWidth,
+      height: videoBoxHeight,
       child: Stack(
+        alignment: Alignment.center,
         children: [
-          Transform(
-            transform: Matrix4.identity()..rotateY(_isInverted == true ? 3.14 : 0.0,),
-            alignment: FractionalOffset.center,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(5.0,),
-              child: widget.streamData["widget"],
-            ),
-          ),
+          widget.streamData["status"]["video"] == true ? videoBox() : videoCircle(),
           ButtonView.hover(
             onPressed: widget.onPressed,
             onHover: ContainerChanges(
@@ -74,7 +85,7 @@ class _StreamWidgetState extends State<StreamWidget> {
             child: ButtonView(
               onPressed: (){
                 setState(() {
-                  _isInverted = !_isInverted;
+                  widget.streamData["switch"](!widget.streamData["is inverted"], widget.streamData["id"]);
                 });
               },
               padding: EdgeInsets.all(5.0,),
@@ -84,9 +95,43 @@ class _StreamWidgetState extends State<StreamWidget> {
               ),
             ),
           ),
+          if(widget.streamData["status"]["audio"] == false)
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                padding: EdgeInsets.all(5.0),
+                decoration: BoxDecoration(
+                  color: colors.darkPurple,
+                  borderRadius: BorderRadius.circular(90.0),
+                ),
+                child: Icon(Icons.mic_off, size: 25.0, color: colors.white,),
+              ),
+            )
         ],
       ),
     );
   }
 
+  Widget videoBox() {
+    return Transform(
+      transform: Matrix4.identity()..rotateY(widget.streamData["is inverted"] == true ? 3.14 : 0.0,),
+      alignment: FractionalOffset.center,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(5.0,),
+        child: widget.streamData["widget"],
+      ),
+    );
+  }
+
+  Widget videoCircle() {
+    double diameter = videoBoxWidth > videoBoxHeight ? videoBoxHeight : videoBoxWidth;
+    return ClipOval(
+      child: ImageView.network(
+        imageKey: "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fHBlb3BsZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        aspectRatio: 1.0,
+        width: diameter,
+        height: diameter,
+      ),
+    );
+  }
 }
